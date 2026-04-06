8117975efc103f274ee7f6ee277ef431a090cf0d
nababanalmaidah <nababanalamaidah@gmail.com>
2026-04-06 20:25:47 +0700

    commit 3

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -20,7 +20,26 @@ int main() {
         cin >> kamu[2];
         cout << "Data index 3 = ";
         cin >> kamu[3];
-        cout << "Data index 4 = ";
+        cout << "Data index 4 = ";commit 3
         cin >> kamu[4];
         cout << end1;
+        // menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+
+    // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
            
