47ae1ba0011660b67826cdee212023a47f88f126
Almaidah Nababan <nababanalmaidah.com>
2026-04-06 18:36:33 +0700

    pesan commit anda

---
+++ arrayNew.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        // deklarasi dengan inisialisasi
+        int aku[5] = {10,20,30,40,50};
+        // deklarasi array tanpa inisialisasi
+        int kamu [5];
+        // tampilkan data (membaca data array)
+        cout << "Data pada index 1 = " << aku[1] << end1;
+        // ganti index ke 1(menulis data array)
+        aku[1] = 200;
+        cout << end1;
+        cout << "Data pada index 1 = " << aku[1] << end1;
+        cout << "Data index 0 = ";
+        cin >> kamu[0];
+        cout << "Data index 1 = ";
+        cin >> kamu[1];
+        cout << "Data index 2 = ";
+        cin >> kamu[2];
